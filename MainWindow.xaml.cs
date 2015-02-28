09f3352643c1edfc5dbe9d48ac641919fb2017a0
Padla <root@proger.pp.ua>
2015-02-28 12:24:01 +0200

    Connect true))

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,6 +15,10 @@ using System.Windows.Shapes;
 using Un4seen.Bass;
 using Un4seen.BassWasapi;
 using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Exception;
+using System.IO;
+
 
 namespace VKAll
 {
@@ -22,11 +26,43 @@ namespace VKAll
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {   
         public MainWindow()
         {
             InitializeComponent();
+            txtEMail.Text = Properties.Settings.Default.eMail_set;
+            txtPass.Text = Properties.Settings.Default.pass_set;
+            
+        }
+
+        private void btnAutorizet_Click(object sender, RoutedEventArgs e)
+        {
+            int appID = 4804605;
+            string email = txtEMail.Text;
+            string pass = txtPass.Text;
+            Settings scope = Settings.Friends;
+
             var VK = new VkApi();
+            
+            try
+            {
+                VK.Authorize(appID, email, pass, scope);
+                lblConnStat.Background = Brushes.Green;
+            }
+            catch (VkApiAuthorizationException)
+            {
+                
+                //TypeInitializationException ex
+                //MessageBox.Show(ex.InnerException.ToString());
+                lblConnStat.Background = Brushes.Red;
+            }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.pass_set = txtPass.Text;
+            Properties.Settings.Default.eMail_set = txtEMail.Text;
+            Properties.Settings.Default.Save();
         }
     }
 }
